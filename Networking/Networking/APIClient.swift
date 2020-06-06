c2b4720c3c1dda9e4e7ed1eeac9741ba2ebc0564
Ville Välimaa <ville@wiredcraft.com>
2020-06-06 12:17:18 +0800

    chore: updated gitignore

---
+++ Networking/Networking/APIClient.swift
@@ -0,0 +1,9 @@
+//
+//  APIClient.swift
+//  Networking
+//
+//  Created by Ville Välimaa on 2020/6/6.
+//  Copyright © 2020 Ville Välimaa. All rights reserved.
+//
+
+import Foundation
---
+++ Networking/Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Ville Välimaa on 2020/6/6.
+//  Copyright © 2020 Ville Välimaa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
