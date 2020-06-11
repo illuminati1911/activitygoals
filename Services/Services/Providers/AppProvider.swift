3acc265fb83839ddb55e0a7f1fd6275bd16dad08
Ville Välimaa <ville@wiredcraft.com>
2020-06-11 13:30:37 +0800

    docs: added comments to services module

--- Services/Services/Providers/AppProvider.swift
+++ Services/Services/Providers/AppProvider.swift
@@ -18,6 +18,9 @@ public class AppProvider: MainProvider {
     public var dataProvider: DataProvider
     public var activityProvider: ActivityProvider
 
+    // Services module here intentionally depends on Networking and LocalStorage modules.
+    // This could be changed in the future to use Core module provided interfaces so that all module only depend on the Core
+    //
     init() {
         self.activityProvider = HealthKitActivityProvider(activityService: HKHealthStore())
         self.dataProvider = RemoteWithLocalDataProvider(
